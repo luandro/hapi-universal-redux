4a3f9fe7fe40fa3dbdc586a917f8b68da22c6dba
Luandro <luandro@gmail.com>
2015-12-29 16:50:45 -0200

    fixed DevTools keyboard keys

--- webpack.client-watch.js
+++ webpack.client-watch.js
@@ -23,26 +23,26 @@ config.plugins = [
 
 config.module.loaders =  [
 	{
-      test: /\.js?$/,
-      loader: 'babel',
-      exclude: /node_modules/,
-      query: {
-        "stage": 0,
-        "plugins": ["react-transform"],
-        "extra": {
-          "react-transform": {
-            "transforms": [{
-              "transform": "react-transform-hmr",
-              "imports": ["react"],
-              "locals": ["module"]
-            }, {
-              "transform": "react-transform-catch-errors",
-              "imports": ["react", "redbox-react"]
-            }]
-          }
-        }
-      }
-    }
+			test: /\.js?$/,
+			loader: 'babel',
+			exclude: /node_modules/,
+			query: {
+				"stage": 0,
+				"plugins": ["react-transform"],
+				"extra": {
+					"react-transform": {
+						"transforms": [{
+							"transform": "react-transform-hmr",
+							"imports": ["react"],
+							"locals": ["module"]
+						}, {
+							"transform": "react-transform-catch-errors",
+							"imports": ["react", "redbox-react"]
+						}]
+					}
+				}
+			}
+		}
 ];
 
 config.devServer = {
