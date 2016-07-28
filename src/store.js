7494fd41810413ceb15e6ebc0929079d25648d5a
Luandro <luandro@gmail.com>
2016-07-28 09:24:15 -0300

    change in nextRootReducer in the store #100

--- src/store.js
+++ src/store.js
@@ -13,7 +13,7 @@ export default function (initialState) {
 	if (module.hot) {
 	    // Enable Webpack hot module replacement for reducers
 	    module.hot.accept('./reducers', () => {
-	      	const {reducer: nextReducer} = require('./reducers/index');
+	      	const nextRootReducer = require('./reducers/index').default;
 	      	store.replaceReducer(nextRootReducer);
 	    });
 	}
